7a033b5bab15b0ebbb7051527f6e1aa6f716ec24
Kelsie Nakasone <Kelsie Nakasone@DESKTOP-45T62PH.lan>
2021-04-18 22:14:49 -0700

    General Updates to View and Test classes. Made changes to main to test out the view

--- src/TetrisMain.java
+++ src/TetrisMain.java
@@ -7,8 +7,9 @@ public class TetrisMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//new TetrisView();
+		new TetrisView();
 		new TetrisModel();
+		
 	}
 
 }
--- src/View/TetrisView.java
+++ src/View/TetrisView.java
@@ -22,19 +22,38 @@ public class TetrisView {
 	JPanel panel = new JPanel();
 	JButton button = new JButton("Tetris");
 	
-	panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-	panel.setLayout(new GridLayout(10, 10));
-	panel.setBackground(Color.gray);
+	//Set the size of the GUI
+	//panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
+	//panel.setLayout(new GridLayout(10, 10));
+	//panel.setBackground(Color.gray);
 	
 	//sets original size of the window
 	frame.setPreferredSize(new Dimension(500,500));
 	frame.add(panel, BorderLayout.CENTER);
+	
+	//cannot resize the window
+	frame.setResizable(false);
+	
+	//If you close the window, the program will stop
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setTitle("Tetris but aesthetic?");
+	
+	//Title of the window
+	frame.setTitle("Tetris but with a better color palette");
+	
+	//make the GUI the same size as the game
 	frame.pack();
+	
+	//Position GUI to center of screen
+	frame.setLocationRelativeTo(null);
+	
+	//make window visible to user
 	frame.setVisible(true);
 	
+	//adds button to view
 	panel.add(button);
+	
+	
+
 	}
 	
 	
